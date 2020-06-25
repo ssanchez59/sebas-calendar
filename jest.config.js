3ddf7ab07dc33484d63884f45f45b54b6c3b71ee
Sebastian <sebastian@aktek.io>
2020-06-25 13:04:09 -0500

    tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ const { defaults } = require('jest-config');
 module.exports = {
   // ...
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   // ...
 };
