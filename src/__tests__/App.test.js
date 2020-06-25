c6e896a2db1de09f199ebe562ad878d83458854d
Sebastian <sebastian@aktek.io>
2020-06-25 12:45:51 -0500

    tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from '../app/store';
-import App from '../App';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-test('Renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
