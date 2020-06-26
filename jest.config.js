a66fe1dbe9d0f7e2121dded05966e7704f34a2ed
Sebastian <sebastian@aktek.io>
2020-06-26 08:54:39 -0500

    fixing jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  verbose: true,
-  rootDir: '../',
-  transform: {
-    '^.+\\.js?$': 'babel-jest',
+  automock: false,
+  browser: true,
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss|less)$': 'identity-obj-proxy',
   },
+  moduleFileExtensions: ['js', 'jsx', 'css', 'png'],
+  moduleDirectories: ['node_modules'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'jsdom',
+  roots: ['./src/__tests__'],
+  testRegex: '.*.test.js',
+  verbose: true,
+  rootDir: './',
 };
