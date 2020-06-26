ae4b5f3442ba253dae921ddd641a7346c8458b62
Sebastian <sebastian@aktek.io>
2020-06-26 08:44:04 -0500

    fixing directory route

--- src/__tests__/Reminder.test.js
+++ src/__tests__/Reminder.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Reminder } from '../features/calendar/Reminder';
+import { Reminder } from '../features/reminder/Reminder';
 
 configure({ adapter: new Adapter() });
 
